3c0f0becbcd0c92c95771136a9e1e87bea66eea6
Mayank Mehta <mehta26mayank@gmail.com>
2025-01-13 01:31:23 +0530

    added about page

---
+++ components/About.tsx
@@ -0,0 +1,49 @@
+import { Paragraph } from "@/components/Paragraph";
+
+export default function About() {
+  return (
+    <div>
+      <div className="max-w-4xl">
+        <Paragraph className=" mt-4">
+          Hey there, I&apos;m Mayank Mehta - a passionate developer, who used to read specific stuff, play chess & tennis in its free time. Welcome to my corner of the
+          digital world!
+        </Paragraph>
+        <Paragraph className=" mt-4">
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+        </Paragraph>
+
+        <Paragraph className=" mt-4">
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+        </Paragraph>
+
+        <Paragraph className=" mt-4">
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+        </Paragraph>
+
+        <Paragraph className=" mt-4">
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+        </Paragraph>
+
+        <Paragraph className=" mt-4">
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+        </Paragraph>
+
+        <Paragraph className=" mt-4">
+          Thank you for joining me on this journey—I’m truly grateful to have your support
+        </Paragraph>
+      </div>
+    </div>
+  );
+}

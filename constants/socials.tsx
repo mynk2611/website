a98ec0f04fb92195acc663d13e6d86e3b0418ecd
Mayank Mehta <mehta26mayank@gmail.com>
2025-01-12 13:51:56 +0530

    added navlinks & socials to the constants

---
+++ constants/socials.tsx
@@ -0,0 +1,23 @@
+import {
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconBrandGithub,
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "https://x.com/mame_2611",
+    label: "Twitter",
+    icon: IconBrandTwitter,
+  },
+  {
+    href: "https://linkedin.com/in/mayank-mehta-91754426b/",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://github.com/mynk2611",
+    label: "GitHub",
+    icon: IconBrandGithub,
+  },
+]

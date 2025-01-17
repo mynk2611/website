d3e721ba023c722e284af8a4e79a8560db258658
Mayank Mehta <mehta26mayank@gmail.com>
2025-01-17 15:35:42 +0530

    added mdx config files

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+import remarkGfm from "remark-gfm";
+import createMDX from "@next/mdx";
+
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "res.cloudinary.com"],
+  },
+  experimental: {
+    mdxRs: true,
+  },
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+};
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);

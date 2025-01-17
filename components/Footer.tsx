3608205ff013ed69c7e274fa869b3cc85a762296
Mayank Mehta <mehta26mayank@gmail.com>
2025-01-17 15:36:00 +0530

    update footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,9 @@
-"use client";
 import React from "react";
 
 export const Footer = () => {
   return (
     <div className="p-4 text-center justify-center text-xs text-neutral-500 border-t border-neutral-100">
-      <span className="font-semibold">{new Date().getFullYear()} </span>
+      <span className="font-semibold">2025</span>
       &#8212; Built by Mayank Mehta
     </div>
   );

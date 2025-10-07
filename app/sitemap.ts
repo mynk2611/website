7c696e343f267dcbb6e4db1466bf50dd7ccb152c
Mayank Mehta <mehta26mayank@gmail.com>
2025-10-07 12:24:44 +0530

    update all links

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://portfolio.mayankmehta.tech",
+      url: "https://portfolio.mayankmehta.dev",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://portfolio.mayankmehta.tech/about",
+      url: "https://portfolio.mayankmehta.dev/about",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://portfolio.mayankmehta.tech/projects",
+      url: "https://portfolio.mayankmehta.dev/projects",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://portfolio.mayankmehta.tech/blog",
+      url: "https://portfolio.mayankmehta.dev/blog",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://portfolio.mayankmehta.tech/contact",
+      url: "https://portfolio.mayankmehta.dev/contact",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
